53c987f828c08885c077a0ee707ee341cf13c847
Pedro Rodrigues <48807462+IELSK@users.noreply.github.com>
2021-08-03 02:50:06 -0300

    Delete live directory

--- live/live/src/main/java/com/dio/live/repository/WorkingDayRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dio.live.repository;
-
-import com.dio.live.model.WorkingDay;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WorkingDayRepository extends JpaRepository<WorkingDay, Long> {
-
-}
